2b210c1d730093fbc1a8a19608b711759c7b4b81
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-05-29 23:56:24 -0400

    Create Ejercicio_10.cpp

---
+++ Ejercicio_10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int invertirNumero(int num) {
+    int invertido = 0;
+    while (num > 0) {
+        invertido = invertido * 10 + num % 10;
+        num /= 10;
+    }
+    return invertido;
+}
+int main() {
+    int numero;
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+    cout << "Numero invertido: " << invertirNumero(numero) << endl;
+}
