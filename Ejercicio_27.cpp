b71ab98328715090bca0516ffa82e75a5ffb7f7f
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-06-05 16:53:11 -0400

    Create Ejercicio_27.cpp

---
+++ Ejercicio_27.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void llenarEspiral(int matriz[10][10]) {
+    int valor = 1, minFila = 0, maxFila = 9, minCol = 0, maxCol = 9;
+    while (valor <= 100) {
+        for (int j = minCol; j <= maxCol; j++)
+            matriz[minFila][j] = valor++;
+        minFila++;
+        for (int i = minFila; i <= maxFila; i++)
+            matriz[i][maxCol] = valor++;
+        maxCol--;
+        for (int j = maxCol; j >= minCol; j--)
+            matriz[maxFila][j] = valor++;
+        maxFila--;
+        for (int i = maxFila; i >= minFila; i--)
+            matriz[i][minCol] = valor++;
+        minCol++;
+    }
+}
+
+void mostrarMatriz(int matriz[10][10]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << matriz[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int matriz[10][10];
+    llenarEspiral(matriz);
+    mostrarMatriz(matriz);
+}
